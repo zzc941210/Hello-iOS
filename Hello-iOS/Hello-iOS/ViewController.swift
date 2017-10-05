b9aac941188b9b3d804c569f74b1dc4b69ad9356
Jeff <zhengzichu@gmail.com>
2017-10-05 14:45:55 -0700

    Init commit
    + Project name Hello-iOS
    + Target it to iOS iPhone 5
    + Display "Go Seahawks!"
    + App name on homescreen is “hello”
    + It builds correctly
    + It runs inside the emulator
    + It runs on an iPhone5
    + Screen include

---
+++ Hello-iOS/Hello-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by zichu zheng on 10/5/17.
+//  Copyright © 2017 zichu zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
